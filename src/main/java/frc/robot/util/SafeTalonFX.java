6de29998683ab6cd09a4dec15c78e1e94506750e
Egan Schafer <eganschafer@gmail.com>
2024-02-03 14:50:14 -0700

    Convert SafeTalonFX to pure java

---
+++ src/main/java/frc/robot/util/SafeTalonFX.java
@@ -0,0 +1,63 @@
+package frc.robot.util;
+
+import com.ctre.phoenix.motorcontrol.*;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import frc.robot.Constants;
+import java.lang.Math;
+
+/**
+ * Wrapper used to prevent Falcon 500 from burning out or being damaged
+ * @param deviceNumber Int
+ * @param usePID Boolean: By default this value is false.
+ * @param isDrivetrain Boolean: By default this value is true.
+ */
+public class SafeTalonFX  extends WPI_TalonFX {
+
+    private double maxSpeed = 21000.0;
+    private double deadBand = 0.02;
+    private boolean usePID;
+
+    public SafeTalonFX(int deviceNumber, boolean isDrivetrain, boolean usePID) {
+	super(deviceNumber);
+        configFactoryDefault();
+
+	this.usePID = usePID;
+
+        if (isDrivetrain) {
+//            configStatorCurrentLimit(StatorCurrentLimitConfiguration(true, 20.0, 25.0, 1.0))
+//            configSupplyCurrentLimit(SupplyCurrentLimitConfiguration(true, 10.0, 15.0,0.5))
+//            configStatorCurrentLimit(StatorCurrentLimitConfiguration(true, 40.0, 25.0, 1.0))
+//            configSupplyCurrentLimit(SupplyCurrentLimitConfiguration(true, 20.0, 15.0,0.5))
+        } else {
+            configStatorCurrentLimit(new StatorCurrentLimitConfiguration(true, 100.0, 75.0, 1.0));
+            configSupplyCurrentLimit(new SupplyCurrentLimitConfiguration(true, 80.0, 60.0,0.5));
+        }
+
+        configNeutralDeadband(deadBand);
+
+        setStatusFramePeriod(StatusFrameEnhanced.Status_13_Base_PIDF0, 10, Constants.kTIMEOUT_MS);
+        setStatusFramePeriod(StatusFrame.Status_10_MotionMagic, 10, Constants.kTIMEOUT_MS);
+
+        // configStatorCurrentLimit(config);
+        configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+        configNominalOutputForward(0.0, 30);
+        configNominalOutputReverse(0.0, 30);
+        configPeakOutputForward(1.0, 30);
+        configPeakOutputReverse(-1.0, 30);
+        setSelectedSensorPosition(0.0, Constants.kPIDLoopIdx, Constants.kTIMEOUT_MS);
+    }
+
+    /**
+     * Moves the motor at a percent of its max speed
+     * @param percentOutput A double from -1 to 1
+     */
+    //@Override 
+    public void set(Double percentOutput) {
+        if (usePID && Math.abs(percentOutput) > 0.1) {
+            super.set(ControlMode.Velocity, percentOutput * maxSpeed);
+//            println("INFO: velocity: ${percentOutput * maxSpeed}")
+        } else {
+            super.set(percentOutput);
+        }
+    }
+}

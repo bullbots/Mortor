f3f6fbcd0dea5abb6dd6d05883f85d022b56cd84
Egan Schafer <eganschafer@gmail.com>
2024-02-03 19:18:15 -0700

    minor fix to IntakeGroup

--- src/main/java/frc/robot/commands/Intake_Commands/IntakeGroup.java
+++ src/main/java/frc/robot/commands/Intake_Commands/IntakeGroup.java
@@ -3,17 +3,17 @@ package frc.robot.commands.Intake_Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.commands.Shooter_Commands.ShooterCargos;
+//import frc.robot.commands.Shooter_Commands.ShooterCargos;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 
-/**
- * Used to run IntakeCargo
- * @param intake: Intake
- * @param intakeVel: Double / The default value is 0.3 if a value is not passed in
- */
 public class IntakeGroup extends ParallelCommandGroup {
 
+    /**
+     * Used to run IntakeCargo
+     * @param intake: Intake
+     * @param intakeVel: Double / The default value is 0.3 if a value is not passed in
+     */
     public IntakeGroup(Intake intake, double intakeVel, double armVel, Shooter shooter) {
         addCommands(
             new IntakeCargos(intake, intakeVel, armVel, shooter),

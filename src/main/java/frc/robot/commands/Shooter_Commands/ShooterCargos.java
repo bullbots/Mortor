eb8d6305ec9e89043629019caf7edb7e170074c1
Egan Schafer <eganschafer@gmail.com>
2024-02-03 19:41:50 -0700

    Converted ShooterCargos command to pure java

--- src/main/java/frc/robot/commands/Shooter_Commands/ShooterCargos.java
+++ src/main/java/frc/robot/commands/Shooter_Commands/ShooterCargos.java
@@ -1,29 +1,38 @@
-package frc.robot.commands.Shooter_Commands
+package frc.robot.commands.Shooter_Commands;
 
-import edu.wpi.first.math.MathUtil
-import edu.wpi.first.wpilibj2.command.CommandBase
-import frc.robot.subsystems.Shooter
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+import java.util.function.DoubleSupplier;
 
 /**
  * ShooterCargo initialize the velocity of the staticShooter
  * @param shooter: StaticShooter
  */
-class ShooterCargos(private val shooter: Shooter, private val static: Boolean, private val dist: () -> Double) : CommandBase() {
+public class ShooterCargos extends CommandBase {
 
-    var velocity = 0.0
+    private double velocity = 0.0;
+    private Shooter m_shooter;
+    private boolean m_staticVal;
+    private DoubleSupplier m_dist;
     // Use addRequirements() here to declare subsystem dependencies.
-    init {
-        addRequirements(shooter)
+    public ShooterCargos(Shooter shooter, boolean staticVal, DoubleSupplier dist) {
+        m_shooter = shooter;
+        m_staticVal = staticVal;
+        m_dist = dist;
+        addRequirements(shooter);
     }
 
     // Called when the command is initially scheduled.
-    override fun initialize() {
+    @Override 
+    public void initialize() {
         // var shooterVel = SmartDashboard.getNumber("StaticShooter", 0.3)
-        if(static) {
-            velocity = dist()
-            shooter.shooterSpinner.set(dist())
+        if(m_staticVal) {
+            velocity = m_dist.getAsDouble();
+            m_shooter.getShooterSpinner().set(m_dist.getAsDouble());
         } else {
             // dist() is in feet
+	    /* TODO:translate this to java
             velocity = when (dist()) {
                 in 0.0..5.0 -> 0.26
                 in 5.0..7.0 -> MathUtil.interpolate(0.38, 0.41, (dist()-5) / 2)
@@ -35,14 +44,18 @@ class ShooterCargos(private val shooter: Shooter, private val static: Boolean, p
                 else -> 0.0
             }
             shooter.shooterSpinner.set(velocity)
+            */
         }
 //        println(velocity) // Debugging Values
     }
 
     // Called every time the scheduler runs while the command is scheduled.
-    override fun execute() {}
+    @Override 
+    public void execute() {}
 
-    override fun end(interrupted: Boolean) { shooter.stop() }
+    @Override 
+    public void end(boolean interrupted) { m_shooter.stop(); }
 
-    override fun isFinished(): Boolean { return false }
+    @Override 
+    public boolean isFinished(){ return false; }
 }
